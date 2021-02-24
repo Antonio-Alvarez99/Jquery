c9d72a4e87fa47ab5d89e2ca8c7b0f0368c22c48
Isaac Wilson @ School <ISAAC.WILSON1811@STU.EDU>
2021-02-23 19:16:39 -0700

    jquery

---
+++ Desktop/Storms.Lands/Jquery45/app.js
@@ -0,0 +1,13 @@
+$(".navItem").css("color","white");
+
+$("#nav").css('background-color', '#145');
+
+
+let parent = $("#contentContainer").parents()[1];
+let evenSelector = $("li:even").css('background-color', '#ccc');
+let oddSelector = $("li:odd").css('background-color', '#aaa');
+$("input").val('Antonio');
+
+
+
+
